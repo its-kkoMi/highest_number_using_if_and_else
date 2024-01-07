94ba008cba82c6bd2ad7ca19ae43d63c7f9be93f
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-07 19:17:42 +0800

    Added inputs for the user

--- highest_number_using_if_and_else.py
+++ highest_number_using_if_and_else.py
@@ -8,13 +8,24 @@ from tkinter import messagebox
 
 # - GUI using Tk
 app = tk.Tk()
-app.title("Maximum Number Finder")
+app.title("Highest Number")
 
 frame = tk.Frame(app)
 frame.pack(padx=10, pady=10)
 
 # - Ask user to input three numbers
+input_first_number = tk.Entry(frame)
+input_first_number.grid(row=0, column=1)
+
+input_second_number = tk.Entry(frame)
+input_second_number.grid(row=1, column=1)
+
+input_third_number = tk.Entry(frame)
+input_third_number.grid(row=2, column=1)
+
 # - Validate if the inputs are numeric
 # - Calculate using if-else the highest input
 # - Make pop up messages for the highest input and error input
-# - Labels and Buttonsb
+# - Entry Labels and Button
+
+app.mainloop()
