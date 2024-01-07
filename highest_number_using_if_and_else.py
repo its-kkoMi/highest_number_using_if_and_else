1aaffb12bd1c947983de05eaaecf845d056caa25
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-07 19:12:11 +0800

    Imported Tkinter and made GUI

--- highest_number_using_if_and_else.py
+++ highest_number_using_if_and_else.py
@@ -3,7 +3,16 @@
 #3Find and print the biggest number using only if-else statement
 
 # - Import TK
+import tkinter as tk
+from tkinter import messagebox
+
 # - GUI using Tk
+app = tk.Tk()
+app.title("Maximum Number Finder")
+
+frame = tk.Frame(app)
+frame.pack(padx=10, pady=10)
+
 # - Ask user to input three numbers
 # - Validate if the inputs are numeric
 # - Calculate using if-else the highest input
