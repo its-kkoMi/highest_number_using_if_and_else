301a788a9c236858da58c37a4baceb283a7fbc60
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-07 19:07:27 +0800

    Program Instructions and Pseudocode

---
+++ highest_number_using_if_and_else.py
@@ -0,0 +1,11 @@
+#Assignment 4
+#Ask user to input 3 numbers.
+#3Find and print the biggest number using only if-else statement
+
+# - Import TK
+# - GUI using Tk
+# - Ask user to input three numbers
+# - Validate if the inputs are numeric
+# - Calculate using if-else the highest input
+# - Make pop up messages for the highest input and error input
+# - Labels and Buttonsb
